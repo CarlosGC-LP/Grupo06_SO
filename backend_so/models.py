65db195b93ddefda4f73d8e813a6460f831e9f1e
davidalexd <davidalexd1234@gmail.com>
2024-09-22 03:20:22 -0500

    add urlProducto

--- backend_so/models.py
+++ backend_so/models.py
@@ -5,7 +5,7 @@ class Producto(models.Model):
     descripcion = models.TextField(blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     cantidad_en_stock = models.IntegerField()
-
+    urlImgProducto = models.URLField(max_length=200, blank=True, null=True)
     def __str__(self):
         return self.nombre
 
